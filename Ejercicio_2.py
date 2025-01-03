4af5994d3ce54f99f7c21d962865d8f7013d70c9
Juan Pablo Grajales Ciro <jgrajales@unal.edu.co>
2025-01-03 16:04:09 -0500

    Ejercicio_2.py

---
+++ Ejercicio_2.py
@@ -0,0 +1,13 @@
+suma = 0
+# Asignamos el valor 20 a X
+x = 20
+# Sumamos X a suma
+suma = suma + x
+# Asignamos el valor 40 a Y
+y = 40
+# Calculamos X + Y elevado al cuadrado y actualizamos el valor de X
+x = x + y ** 2
+# Calculamos la suma final con X dividido entre Y
+suma = suma + x / y
+# Mostramos los resultados
+print("El valor de la suma es:", suma)
