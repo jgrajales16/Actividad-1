6144210f89648843194969c757804446122fb14f
Juan Pablo Grajales Ciro <jgrajales@unal.edu.co>
2025-01-03 16:05:18 -0500

    Ejercicio_4.py

---
+++ Ejercicio_4.py
@@ -0,0 +1,8 @@
+# Pedimos al usuario que ingrese un número
+numero = float(input("Ingrese un número: "))
+# Calculamos el cuadrado del número
+cuadrado = numero ** 2
+# Calculamos el cubo del número
+cubo = numero ** 3
+# Mostramos los resultados
+print("El cuadrado del número es:", cua
